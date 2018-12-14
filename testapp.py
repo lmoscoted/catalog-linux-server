2a0211ca1b2fb3c8158e51bda658c49aa1ca8d80
Luis Moscote <ing.luis.moscote@gmail.com>
2018-12-13 22:22:33 -0300

    Remove host specification

--- testapp.py
+++ testapp.py
@@ -2,12 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/catalog')
+# @app.route('/catalog')
 @app.route('/')
 def test():
 	return "It works !!"
 
 if __name__ =='__main__':
 	app.secret_key = 'super_secret_key'
-    app.run(host='www.35.247.193.231.xip.io', port=80)
+    #app.run(host='www.35.247.193.231.xip.io', port=80)
+    app.run()
 
