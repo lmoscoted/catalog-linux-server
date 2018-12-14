8eccd58fc1c22b7ba43e56f5f4e6a57e3b27e68d
Luis Moscote <ing.luis.moscote@gmail.com>
2018-12-14 13:31:16 -0300

    Update server setting

--- application.py
+++ application.py
@@ -601,5 +601,6 @@ def createUser(login_session):
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    #app.debug = True
+    #app.run(host='0.0.0.0', port=5000)
+    app.run()
